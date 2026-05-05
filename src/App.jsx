f962128f21bf87af37d9a199097b456f6d8bdcb7
YinXuan <tanyinxuanxuan@gmail.com>
2026-05-05 23:19:47 +0800

    Dashboard Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import ROICalculator from "./components/ROIcalculator";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
+import DashboardPage from "./pages/DashboardPage";
 
 function AppLayout() {
   const location = useLocation();
 
   const hideNavbar =
-    location.pathname === "/" ||
     location.pathname === "/login" ||
     location.pathname === "/register";
 
@@ -15,7 +15,8 @@ function AppLayout() {
       {!hideNavbar && <Navbar />}
 
       <Routes>
-
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/calculator" element={<ROICalculator />} />
       </Routes>
     </>
