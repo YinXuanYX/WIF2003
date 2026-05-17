d7eadc8a7a8c16e004ae329cb02def42e6a17b16
Tzexuan <cheng050728@gmail.com>
2026-05-17 23:12:03 +0800

    implement scoring functionality

--- server/controllers/investmentController.js
+++ server/controllers/investmentController.js
@@ -1,7 +1,31 @@
-export const submitInvestmentProfile = async (_req, res, next) => {
+import User from '../models/User.js';
+import { scoreToProfile, getProfileAllocation } from '../utils/investmentProfile.js';
+
+export const submitInvestmentProfile = async (req, res, next) => {
   try {
-    return res.status(501).json({
-      message: 'Investment profile scoring is not implemented yet.',
+    const { score } = req.body;
+    const userId = req.user._id;
+
+    const profile = scoreToProfile(score);
+    const allocation = getProfileAllocation(profile);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        'riskProfile.profile': profile,
+        'riskProfile.score': score,
+        'riskProfile.allocation': allocation,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    return res.status(200).json({
+      profile: updatedUser.riskProfile.profile,
+      score: updatedUser.riskProfile.score,
+      allocation: updatedUser.riskProfile.allocation,
     });
   } catch (error) {
     next(error);
